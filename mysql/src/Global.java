a54155a0e864177ac379a165baa82974ec430d8b
dany <129038840+xdanyzy@users.noreply.github.com>
2025-11-09 17:52:24 +0800

    Delete mysql directory

--- mysql/src/Global.java
+++
@@ -1,6 +0,0 @@
-public class Global {
-    final static String URL = "jdbc:mysql://localhost:3306/sistembank";
-	final static String USERNAME = "root";
-	final static String PW = "";
-	final static String DB = "bank";
-}
